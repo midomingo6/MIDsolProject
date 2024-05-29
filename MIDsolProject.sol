f7d2278a1690e11c6b14fc5012fb843c93bf2ae4
midomingo6 <167491079+midomingo6@users.noreply.github.com>
2024-05-29 16:19:12 +0800

    Update MIDsolProject.sol

--- MIDsolProject.sol
+++ MIDsolProject.sol
@@ -3,24 +3,20 @@ pragma solidity 0.8.18;
 
 contract MIDsolProject {
 
-    // public variables here
-    string public tokenName = "Mau Iv Dom";
-    string public tokenAbbrv = "MID";
+    string public tokenName = "MauIvDom Chain";
+    string public tokenAbbrv = "MIDC";
     uint public totalSupply = 0;
 
-    // mapping variable here
     mapping(address => uint) public balances;
 
-    // mint function
     function mint(address _address, uint _value) public {
-        require(_value > 0, "Insufficient balance to mint");
+        require(_value > 0, "Insufficient value to mint");
         totalSupply += _value;
         balances[_address] += _value;
     }
 
-    // burn function
     function burn(address _address, uint _value) public {
-        require(_value > 0, "Insufficient balance to burn");
+        require(_value > 0, "Insufficient value to burn");
         require(balances[_address] >= _value, "Insufficient balance to burn");
         totalSupply -= _value;
         balances[_address] -= _value;
